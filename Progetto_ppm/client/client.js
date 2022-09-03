b8c0f9a4a57262d68a8fdaaa1536ecd6ff8a5495
Lollo999 <lolopinna99@gmail.com>
2022-09-03 10:30:41 +0200

    start game when both clients press start

--- Progetto_ppm/client/client.js
+++ Progetto_ppm/client/client.js
@@ -1,1 +1,20 @@
+
+const onGameStart = () => {
+    console.log('start command received')
+    window.location = "/questions.html";
+    console.log('page changed');
+  };
+  
+
+
+const onReadyPressed = (e) =>{
+    e.preventDefault();
+    //TODO when button gets pressed
+    document.getElementById('ready').classList.add('disabled')
+    sock.emit('ready', 'something');
+};
+
 const sock = io();
+sock.on("startGame", onGameStart);
+document.querySelector('#ready').addEventListener('click', onReadyPressed);
+
--- Progetto_ppm/server/server.js
+++ Progetto_ppm/server/server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const app = express();
+const path = require("path");
 
 const clientPath = `${__dirname}/../client`;
 console.log(`seving static from ${clientPath}`);
@@ -10,9 +11,20 @@ app.use(express.static(clientPath));
 
 const server = http.createServer(app);
 
+var number = 0;
+
 const io = socketio(server);
 io.on('connection', (sock) =>{
     console.log('user connesso');
+
+    sock.on('ready', ()=>{
+        number++;
+        console.log('ready button pressed: '+number);
+        if(number == 2){
+            io.emit("startGame");
+            console.log('startGame sent');
+        }
+    })
 });
 
 server.on('error',err => {
