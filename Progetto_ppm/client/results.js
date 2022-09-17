e37b1b99b3d4e5f04a09a6c629bd792b69aa84f2
Davide307 <davide307@gmail.com>
2022-09-17 17:24:24 +0200

    one page results

--- Progetto_ppm/client/results.js
+++
@@ -1,5 +0,0 @@
-const sock=io();
-$('#replytest').text("spaghetti");
-sock.on('message',function(message){
-    $('#replytest').text(message);
-  });
--- Progetto_ppm/client/waitingRoom.js
+++
@@ -1,9 +0,0 @@
-const sock = io();
-
-const onResults = () => {
-    console.log('result command received');
-    window.location = "/result_screen.html";
-  };
-  
-  sock.on("results", onResults);
-
