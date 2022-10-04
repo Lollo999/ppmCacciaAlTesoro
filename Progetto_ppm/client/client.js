af2327949b2c387e6bb9e7d4550cb978485599aa
Lollo999 <lolopinna99@gmail.com>
2022-10-04 19:55:09 +0200

    fixed button client.js

--- Progetto_ppm/client/client.js
+++ Progetto_ppm/client/client.js
@@ -1,22 +1,11 @@
-const { disconnect, emit, send } = require("process");
 
-const onGameStart = () => {
-    console.log('start command received');
-    //window.location = "/questions.html";
-  };
-  
-const onReadyPressed = (e) =>{
-    //e.preventDefault(); non serve se si usa il form
-    //TODO when button gets pressed
-    $('#ready').addClass('disabled');
-};
+$(document).ready(function(){
 
-const onTooManyClients = () =>{
-    //modificare la pagina in modo da notificare l'evento
-};
+    const onReadyPressed = (e) =>{
+        console.log("prova");
+        $('#ready').addClass('disabled');
+    };
+    
+    $('#ready').click(onReadyPressed);
+})
 
-
-const sock = io();
-sock.on("startGame", onGameStart);
-sock.on("tooManyClients", onTooManyClients);
-document.querySelector('#ready').addEventListener('click', onReadyPressed);
